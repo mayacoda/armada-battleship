8671775780abba71ebf60e9e8be8bed8b3a659be
Maya Nedeljković Batić <maya.nedeljko@gmail.com>
2022-09-04 13:52:41 +0200

    Enable statically serving client from server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,32 @@
-console.log('hello world')
+import * as path from 'path'
+import Fastify from 'fastify'
+import FastifyStatic from '@fastify/static'
+
+const hostname = 'localhost'
+const port = 3000
+
+const fastify = Fastify({ logger: true })
+
+let root = path.join(process.cwd(), '..', 'client/dist')
+
+fastify.register(FastifyStatic, {
+  root: root,
+})
+
+fastify.get('/', function (req, reply) {
+  reply.type('text/html').sendFile('index.html')
+})
+
+fastify.listen(
+  {
+    port,
+    host: hostname,
+  },
+  (err, address) => {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    fastify.log.info(`server listening on ${address}`)
+  }
+)
